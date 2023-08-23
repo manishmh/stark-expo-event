d00cccbed87e75c8d1f5cc316495dbfe6b9e9eaa
manishmh <manishmh982@gmail.com>
2023-08-23 16:56:24 +0530

    date changed for event

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -13,14 +13,14 @@ const Timeline = () => {
         <div className="flex flex-col md:flex-row items-center">
           <div className="relative">
             <Hepta />
-            <h1 className="absolute -top-4 md:top-10 ml-12 md:ml-0 md:-left-16 w-[170px] mt-4 text-white mb-4 md:mb-0 md:text-xl lg:text-3xl mx-1 text-center md:text-center">Registration closes<br/>25th Aug 12 midnight </h1>
+            <h1 className="absolute -top-4 md:top-10 ml-12 md:ml-0 md:-left-16 w-[170px] mt-4 text-white mb-4 md:mb-0 md:text-xl lg:text-3xl mx-1 text-center md:text-center">Registration closes<br/>02nd Sep 12 midnight </h1>
           </div>
           <span className="border-l-2 md:border-b-2 w-1 h-16 md:h-auto md:w-[12vw] lg:w-[13vw] xl:w-[15vw] border-dashed border-blue-400"></span>
         </div>
         <div className="flex flex-col md:flex-row items-center">
           <div className="relative">
             <Hepta />
-            <h1 className="absolute -top-4 md:top-10 ml-12 md:ml-0 md:-left-16 w-[170px] mt-4 text-white mb-4 md:mb-0 md:text-xl lg:text-3xl mx-1 text-center md:text-center"> Event starts<br/>27th Aug 10am</h1>
+            <h1 className="absolute -top-4 md:top-10 ml-12 md:ml-0 md:-left-16 w-[170px] mt-4 text-white mb-4 md:mb-0 md:text-xl lg:text-3xl mx-1 text-center md:text-center"> Event starts<br/>03rd Sep 10am</h1>
           </div>
           <span className="border-l-2 md:border-b-2 w-1 h-16 md:h-auto md:w-[12vw] lg:w-[13vw] xl:w-[15vw] border-dashed border-blue-400"></span>
         </div>
@@ -34,7 +34,7 @@ const Timeline = () => {
         <div className="flex flex-col md:flex-row items-center">
           <div className="relative">
             <Hepta />
-            <h1 className="absolute -top-4 md:top-10 ml-12 md:ml-0 md:-left-16 w-[170px] mt-4 text-white mb-4 md:mb-0 md:text-xl lg:text-3xl mx-1 text-center md:text-center">Event ends<br/>27th Aug 4pm</h1>
+            <h1 className="absolute -top-4 md:top-10 ml-12 md:ml-0 md:-left-16 w-[170px] mt-4 text-white mb-4 md:mb-0 md:text-xl lg:text-3xl mx-1 text-center md:text-center">Event ends<br/>03 Sep 4pm</h1>
           </div>
           <span className="hidden border-l-2 md:border-b-2 w-1 h-16 md:h-auto md:w-0 lg:w-[13vw] xl:w-[15vw] border-dashed border-blue-400"></span>
         </div>
